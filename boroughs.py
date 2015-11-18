a21eef615e2ea52e25c8c97c3db1a8c7ee3dfc96
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-11-18 15:04:17 -0500

    Update boroughs.py

--- boroughs.py
+++ boroughs.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 """Tasks 1-3."""
 
+
 import csv
 import json
-
 
 GRADES = {
     'A': float(1.00),
@@ -15,99 +15,102 @@ GRADES = {
 }
 
 
-def get_score_summary(filename):
-    """To open and reads csv file and return a summary of data.
-    Args:
-        fhandler: opens a file in read form.
-        line: skips top line from file
-        data(dict): a dict for score data.
-    Returns:
-        None
-    Examples:
-        >>> get_score_summary('inspection_results.csv')
-        >>> {'BRONX': (156, 0.9762820512820514), 'BROOKLYN':
-        (417, 0.9745803357314141), 'STATEN ISLAND': (46, 0.9804347826086955),
-        'MANHATTAN': (748, 0.9771390374331531), 'QUEENS':
-        (414, 0.9719806763285017)}
-    """
-    data = {}
-    fhandler = open(filename, 'r')
-    csv_f = csv.reader(fhandler)
 
-    for row in csv_f:
-        if row[10] not in ['P', '', 'GRADE']:
-            data[row[0]] = [row[1], row[10]]
-            data.update(data)
+def get_score_summary(filename):
+    """A function to loop through CSV data.
+    Args:
+        filename(file): A csv file.
+    Returns:
+        dict: A new dictionary.
+    Examples:
+    >>> get_score_summary('inspection_results.csv')
+    {'BRONX': (156, 0.9762820512820514),
+    'BROOKLYN': (417, 0.9745803357314141),
+    'STATEN ISLAND': (46, 0.9804347826086955),
+    'MANHATTAN': (748, 0.9771390374331531),
+    'QUEENS': (414, 0.9719806763285017)}
+    """
+
+    fhandler = open(filename, 'r')
+    csv_file = csv.reader(fhandler)
+    mydata1 = {}
+
+    for line in csv_file:
+        if line[0] not in mydata1 and line[10] in GRADES:
+            mydata1.update({line[0]: [line[10], line[1]]})
+
     fhandler.close()
 
-    summary = {}
-    for values in data.itervalues():
-        if values['boro'] not in summary:
-            summary[values['boro']] = {'count': 1, 'grade': values['grade']}
-        else:
-            summary[values['boro']]['count'] += 1
-            summary[values['boro']]['grade'] += values['grade']
+    mydata2 = {}
 
+    for item in mydata1.values():
+        try:
+            mydata2[item[1]][0] += 1
+            mydata2[item[1]][1] += GRADES[item[0]]
+        except KeyError:
+            mydata2[item[1]] = [1, GRADES[item[0]]]
+
+    mydata3 = {}
+
+    for boro, grade in mydata2.iteritems():
+        mydata3[boro] = grade[0], grade[1]/grade[0]
+
+    return mydata3
+
+
+def get_market_density(filename):
+    """A function using JSON.
+    Args:
+        filename(file): A json file.
+    Returns:
+        dict: A new dictionary.
+    Example:
+    >>> get_market_density('green_markets.json')
+    {u'Staten Island': 2, u'Brooklyn': 48, u'Bronx': 32,
+    u'Manhattan': 39, u'Queens': 16}
+    """
+
+    fhandler = open(filename, 'r')
+    jsondata = json.load(fhandler)["data"]
     result = {}
-    for boro, data in summary.iteritems():
-        result[boro] = (data['count'], data['grade'] / data['count'])
+    fhandler.close()
+
+    for item in jsondata:
+        boro = item[8].strip()
+
+        try:
+            result[boro] += 1
+
+        except KeyError:
+            result[boro] = 1
 
     return result
 
 
-def get_market_density(filename):
-    """Opens a file using json to store data in a dict.
+def correlate_data(firstarg='inspection_results.csv',
+                   secondarg='green_markets.json',
+                   thirdarg='finalresult.json'):
+    """Combining the two pieces of data.
     Args:
-        fhandler: opens file in read form.
-        data: to loads json file
-        boros(dict): new dict for market density.
+        firstarg(file): A default csv file.
+        secondarg(file): A default json file.
+        thirdarg(file): The resulting json file.
     Returns:
-        market_data (dict) = Market density data.
-    Examples:
-        >>> get_market_density('green_markets.json')
-        {u'STATEN ISLAND': 2, u'BROOKLYN': 48, u'BRONX': 32,
-        u'MANHATTAN': 39, u'QUEENS': 16}
+        JSON writing function that fills in the thirdarg file.
     """
-    fhandler = open(filename)
-    data = json.load(fhandler)['data']
-    fhandler.close()
 
-    boros = {}
-
-    for market in data:
-        boro = market[8].strip().upper()
-        if boro not in boros:
-            boros[boro] = 1
-
-        else:
-            boros[boro] += 1
-
-    return boros
-
-
-def correlate_data(file1='inspection_results.csv',
-                   file2='green_markets.json',
-                   file3='result.json'):
-    """Combines data for restaurants and markets.
-    Args:
-        rdata:(dict): restaurant score summary.
-        mdata(dict):market density summary.
-    Returns:
-        correlated(dict): a combined dict.
-    Example:
-        {'BRONX': (0.9762820512820514, 0.1987179487179487)}
-    """
-    data1 = get_score_summary(file1)
-    data2 = get_market_density(file2)
+    score = get_score_summary(firstarg)
+    market = get_market_density(secondarg)
     result = {}
-    for key2 in data2.iterkeys():
-        for key1 in data1.iterkeys():
-            if key1 == str(key2).upper():
-                val1 = data1[key1][1]
-                val2 = float(data2[key2])/(data1[key1][0])
-                result[key2] = (val1, val2)
-                result.update(result)
-    jdata = json.dumps(result)
-    fhandler = open(file3, 'w')
-    fhandler.write(jdata)
+
+    for key, value in market.iteritems():
+        boro = key.upper()
+
+        if boro in score:
+            result[boro] = score[boro][1], float(value)/float(score[boro][0])
+
+    jsondata = json.dumps(result)
+
+    fhandler = open(thirdarg, 'w')
+    fhandler.write(jsondata)
     fhandler.close()
